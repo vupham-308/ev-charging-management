1eaba9ae32892a78b68e40cabcef6c417adf9639
HuuNhat <thuunhat@gmail.com>
2025-10-07 16:40:29 +0700

    Them CUD cho ADMIN

--- ev-charging-backend/src/main/java/com/ev/evchargingsystem/entity/User.java
+++ ev-charging-backend/src/main/java/com/ev/evchargingsystem/entity/User.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Check;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -37,7 +38,7 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(this.role));
     }
 
     @Override
