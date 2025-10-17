518696012a05bf0306bd34f01f1690371e6753ab
vupham-308 <ptv.vupham@gmail.com>
2025-10-17 18:07:04 +0700

    Add api get charging session for driver

--- ev-charging-backend/src/main/java/com/ev/evchargingsystem/repository/ChargingSessionRepository.java
+++ ev-charging-backend/src/main/java/com/ev/evchargingsystem/repository/ChargingSessionRepository.java
@@ -3,6 +3,8 @@ package com.ev.evchargingsystem.repository;
 import com.ev.evchargingsystem.entity.ChargingSession;
 import com.ev.evchargingsystem.model.request.ChargingSessionRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,4 +12,12 @@ public interface ChargingSessionRepository extends JpaRepository<ChargingSession
     List<ChargingSession> findChargingSessionByStatus(String status);
 
     ChargingSession findChargingSessionById(int sessionId);
+
+    @Query("""
+        SELECT c 
+        FROM ChargingSession c 
+        WHERE c.car.user.id = :userId 
+          AND c.status = 'ONGOING'
+    """)
+    List<ChargingSession> findOngoingSessionsByUser(@Param("userId") int userId);
 }
