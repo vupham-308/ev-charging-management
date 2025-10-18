e5ea23daca85bb868f7845ff164d33be427eecc0
Hieu080304 <hieubqse180374@fpt.edu.vn>
2025-10-19 00:58:10 +0700

    New FE merge BE v.1

--- ev-charging-frontend/src/config/axios.js
+++ ev-charging-frontend/src/config/axios.js
@@ -5,4 +5,13 @@ const api = axios.create({
   baseURL: 'http://222.255.214.35:8080/api/'
 });
 
+
+// Thêm interceptor để tự động thêm header Authorization vào mọi request
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 export default api;
